75e242724088efb0eefb4774e1f5fcbc906298b3
Chandranil Adhikary <chandraniladhikary16dec2003@gmail.com>
2026-09-13 02:25:13 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,9 @@ const app=express();
 const PORT=process.env.PORT
 
 //middleware
-app.use (cors());
+app.use (cors({
+  origin:process.env.FRONTEND_URL,
+}));
 app.use (express.json());
 app.use (express.urlencoded ({extended: true}));
 
